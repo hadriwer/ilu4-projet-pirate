b828b7233ed8c6f1f003fcd584807f96bdc4958d
snwrr <ylala2910@gmail.com>
2025-04-04 17:26:01 +0200

    feat: ajout de l'arborescence des fichiers

--- README.md
+++
@@ -1,3 +0,0 @@
-# ilu4-projet-pirate
-
-## Mail du prof : axel.carayon@irit.fr
---
+++ ppirate/src/main/java/com/mycompany/ppirate/Ppirate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ppirate;
+
+/**
+ *
+ * @author wer
+ */
+public class Ppirate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
