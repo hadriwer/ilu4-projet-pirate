05a47f61cf1a16596465405f407c7dc24aa5c31c
Hadrien Lagier <hadriwer@laposte.net>
2025-04-07 12:18:11 +0200

    feat: ajout de la classe Pioche + idée sur la fonction initPioche()

--- ppirate/src/main/java/model/Pioche.java
+++ ppirate/src/main/java/model/Pioche.java
@@ -4,12 +4,23 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.cartes.Attaque;
+import model.cartes.Carte;
+
 /**
  *
  * @author wer
  */
 public class Pioche {
+    private List<Carte> pioche;
     public Pioche() {
-        
+        pioche = new ArrayList<>();
+        initPioche();
+    }
+    
+    public void initPioche() {
+        pioche.add(new Attaque("Coup de Sabre", "tqt", 10));
     }
 }
