f31aff7e02dbef459fb6dd77c8db4396dd29780d
Zelmine <zelmine31@gmail.com>
2025-04-14 11:37:38 +0200

    feat : Joueur v1

--- ppirate/src/main/java/model/Joueur.java
+++ ppirate/src/main/java/model/Joueur.java
@@ -4,6 +4,7 @@
  */
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 import model.cartes.Carte;
 
@@ -19,16 +20,85 @@ public class Joueur {
     
     public Joueur(String nom, List<Carte> mainJoueur) {
         this.indicePopularite = 0;
-        this.indiceVie = 0;
+        this.indiceVie = 5;
         this.nom = nom;
         this.mainJoueur = new MainJoueur(mainJoueur);        
+    }
+    
+    public int getIndicePopularite(){
+        return this.indicePopularite;
+    }
+    
+    public int getIndiceVie(){
+        return this.indiceVie;
+    }
+    
+    public String getNom(){
+        return this.nom;
+    }
+    
+    public MainJoueur getMainJoueur(){
+        return this.mainJoueur;
+    }
+    
+    public void perdreVie(){
+        if (indiceVie > 0){
+            this.indiceVie --;
+        }
+    }
+    
+    public void gagnerPop(){
+        if(indicePopularite < 5){
+           this.indicePopularite ++; 
+        }
+    }
+    
+    public boolean giveGagnant(){
+        return this.indicePopularite == 5;
+    }
+    
+    public boolean givePerdant(){
+        return this.indiceVie == 0;
+    }
+    
+    @Override
+    public String toString(){
+        return "Joueur : "+this.getNom()+ "\n" + 
+                "Vie : " + this.indiceVie + "\n" +
+                "Popularité : "+this.indicePopularite + "\n" +
+                this.mainJoueur.toString();
     }
     
     private class MainJoueur {
         private List<Carte> mainJoueur;
 
         public MainJoueur(List<Carte> mainJoueur) {
-            this.mainJoueur = mainJoueur;
+            this.mainJoueur = new ArrayList<>();
         }
+        
+        public List<Carte> getMain(){
+            return this.mainJoueur;
+        }
+        
+        public void prendreCarte(Carte carte){
+            this.mainJoueur.add(carte);
+        }
+        
+        public void jouerCarte(Carte carte){
+            if (this.mainJoueur.contains(carte)){
+                this.mainJoueur.remove(carte);
+            }
+        }  
+        
+        @Override
+        public String toString(){
+            String cartes = "Contenu de la main du joueur :";
+            for (Carte c : this.getMain()){
+                cartes += c.toString()+" ";
+            }
+            cartes += "]";
+            return cartes;
+        }
+        
     }
 }
