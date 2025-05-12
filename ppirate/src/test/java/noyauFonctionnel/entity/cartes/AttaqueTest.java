6f0cc1c4845cab53270bed8108b3dcc6b38e28bb
DeChab-Antoine <antoine.de-chabannes-curton-la-palice@univ-tlse3.fr>
2025-05-12 21:48:58 +0200

    feat : couverture du code de la carte Attaque avec le fichier AttaqueTest

--- ppirate/src/test/java/noyauFonctionnel/entity/cartes/AttaqueTest.java
+++ ppirate/src/test/java/noyauFonctionnel/entity/cartes/AttaqueTest.java
@@ -2,13 +2,121 @@ package noyauFonctionnel.entity.cartes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import noyauFonctionnel.entity.Joueur;
+import util.EnumCarte;
+
 
 class AttaqueTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+	private Attaque carteAttaque;
+	private Joueur j1;
+	private Joueur j2;
+	
+    @BeforeEach
+    public void setUp() {
+        carteAttaque = new Attaque(1, "Attaque", "desc", 2, 0);
+        
+        j1 = new Joueur(new ArrayList<>());
+        j2 = new Joueur(new ArrayList<>());
+    }
 
+    
+    @Test
+    public void testGetActionVie() {
+    	assertEquals(2, carteAttaque.getActionVie());
+    }
+    
+    
+    @Test
+    public void testGetSelfDegats() {
+    	assertEquals(0, carteAttaque.getSelfDegats());
+    }
+    
+    
+    @Test
+    public void testToString() {
+        String attendu = "Attaque[nom=Attaque, degat=2, auto-degat=0]";
+        assertEquals(attendu, carteAttaque.toString());
+    }
+    
+    
+    
+    @Test
+    public void testEqualsTrue() {
+    	// cas même référence
+    	assertTrue(carteAttaque.equals(carteAttaque));
+    	
+    	// cas même attributs
+        Attaque memeAttaque = new Attaque(1, "Attaque", "desc", 2, 0);
+        assertTrue(carteAttaque.equals(memeAttaque));
+    }
+    
+    
+    @Test
+    public void testEqualsFalse() {
+    	// cas object null
+    	assertFalse(carteAttaque.equals(null));
+    	
+    	// cas pas une carte
+    	assertFalse(carteAttaque.equals("Pas une carte"));
+    	
+    	// cas nom différent
+        Attaque attaqueDiff1 = new Attaque(2, "AttaqueDiff1", "desc", 2, 0);
+        assertFalse(carteAttaque.equals(attaqueDiff1));
+        
+        // cas effet selfDegat différent
+        Attaque attaqueDiff2 = new Attaque(1, "Attaque", "desc", 2, 1);
+        assertFalse(carteAttaque.equals(attaqueDiff2));
+        
+        // cas effet actionVie différent
+        Attaque attaqueDiff3 = new Attaque(1, "Attaque", "desc", 1, 0);
+        assertFalse(carteAttaque.equals(attaqueDiff3));
+        
+    }
+    
+    
+    @Test
+    public void testHashCode() {
+        Attaque memeAttaque = new Attaque(1, "Attaque", "desc", 2, 0);
+        assertEquals(carteAttaque.hashCode(), memeAttaque.hashCode());
+    }
+    
+    
+    @Test
+    public void testClone() {
+        Attaque copie = carteAttaque.clone();
+        assertEquals(carteAttaque, copie);
+        assertNotSame(carteAttaque, copie);
+    }
+
+    @Test
+    public void testGetType() {
+        assertEquals(EnumCarte.ATTAQUE, carteAttaque.getType());
+    }
+    
+    
+    @Test
+    public void testEffetAppliqueSiNonProtege() {
+        
+        carteAttaque.apply(j1, j2);
+
+        assertEquals(5, j1.getIndiceVie()); 
+        assertEquals(3, j2.getIndiceVie()); 
+    }
+
+    
+    @Test
+    public void testEffetAppliqueSiProtege() {
+    	j2.getCarteProtegeJoueur().add(carteAttaque);
+    	
+        carteAttaque.apply(j1, j2);
+
+        assertEquals(5, j1.getIndiceVie()); 
+        assertEquals(5, j2.getIndiceVie()); 
+    }
 }
