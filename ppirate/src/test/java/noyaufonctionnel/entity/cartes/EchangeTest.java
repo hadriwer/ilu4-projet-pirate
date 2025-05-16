8c0a9fe395c58ddeb404d3eccb1d1396b377594b
Antoine De chabannes curton la palice <dcn4380a@ensfsi.onmicrosoft.com>
2025-05-16 15:49:35 +0200

    feat : rajout couverture manquante echangeTest

--- ppirate/src/test/java/noyaufonctionnel/entity/cartes/EchangeTest.java
+++ ppirate/src/test/java/noyaufonctionnel/entity/cartes/EchangeTest.java
@@ -62,6 +62,7 @@ class EchangeTest {
     
     @Test
     void testSetEffet() {
+    	carteEchange.apply(j1,j2);
     	// code de setEffet de la carte echange pour récuperer les cartes
 		Carte c1 = j1.carteHasard(j1.getMainJoueur().getCartes().size());
 		Carte c2 = j2.carteHasard(j1.getMainJoueur().getCartes().size());
